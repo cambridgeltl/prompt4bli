5f7571dc9415d91aab95fdb4d628bf04e8c48b6e
yaoyiran <yaoyiran@umich.edu>
2023-10-22 10:13:56 +0100

    Update run_training.py

--- run_training.py
+++ run_training.py
@@ -1,7 +1,7 @@
 import os
 
 n_shot = 5
-Model = "facebook/xglm-1.7B"
+Model = "facebook/xglm-1.7B"  # Only 4 smaller-scale LLMs are adopted for fine-tuning in our preliminary experiments: mT5-base, mT5-large, XGLM-564M, and XGLM-1.7B. 
 size_train = "5k"  # or "1K" 
 batch_size_train = 16  # 16 for XGLM-1.7B, 32 for XGLM-564M and mT5-base/large
 learning_rate = 5e-9  # 2e-6 for mT5-base, 1e-6 for mT5-large, 5e-8 for XGLM-564M, 5e-9 for XGLM-1.7B in the 5K setup, 2e-8 for XGLM-1.7B in the 1K setup 
