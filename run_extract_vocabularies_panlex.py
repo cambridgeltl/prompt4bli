c1a29d0c1e9eb5d4538899a5770574cf3c7a441e
yaoyiran <yaoyiran@umich.edu>
2023-12-17 23:01:00 +0800

    Update run_extract_vocabularies_panlex.py

--- run_extract_vocabularies_panlex.py
+++ run_extract_vocabularies_panlex.py
@@ -6,7 +6,7 @@ for lang in langs:
     print(lang)
     sys.stdout.flush()
     EMB_DIR = "/media/data/WESPLX/fasttext.cc.{}.300.vocab_200K.vec".format(lang) 
-    SAVE_ROOT = "/media/data/T2TData/" # save aligend WEs
+    SAVE_ROOT = "/media/data/T2TData/" # save dir
 
     os.system('python ./src/extract_vocabularies.py --lang {} --emb_dir {} --save_dir {}'.format(lang, EMB_DIR, SAVE_ROOT))
 
