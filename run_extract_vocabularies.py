ef467b5cbb5c2d3756b78ce0c389c26951c66c91
yaoyiran <yaoyiran@umich.edu>
2023-12-17 23:00:40 +0800

    Update run_extract_vocabularies.py

--- run_extract_vocabularies.py
+++ run_extract_vocabularies.py
@@ -7,7 +7,7 @@ for lang in langs:
     sys.stdout.flush()
  
     EMB_DIR = "/media/data/WES/fasttext.wiki.{}.300.vocab_200K.vec".format(lang)
-    SAVE_ROOT = "/media/data/T2TData/" # save aligend WEs
+    SAVE_ROOT = "/media/data/T2TData/" # save dir
 
     os.system('python ./src/extract_vocabularies.py --lang {} --emb_dir {} --save_dir {}'.format(lang, EMB_DIR, SAVE_ROOT))
 
